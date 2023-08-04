2be88df85c6e94eda7079679c971d37a1f298e52
Markus Gerdes <markus.gerdes@inovex.de>
2023-08-04 12:52:27 +0200

    test defaults for auth storage config

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -3,12 +3,12 @@ package config_test
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"github.com/stretchr/testify/suite"
 	"github.com/inovex/CalendarSync/internal/config"
 	"github.com/inovex/CalendarSync/internal/sync"
 	"github.com/inovex/CalendarSync/internal/transformation"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
 type ConfigTestSuite struct {
@@ -45,4 +45,14 @@ func (suite *ConfigTestSuite) TestLoadingTransformersFromFile() {
 	replaceTitle := loadedTransformers[4].(*transformation.ReplaceTitle)
 	assert.NotNil(suite.T(), replaceTitle.Name())
 	assert.Equal(suite.T(), "[Synchronisierter Termin]", replaceTitle.NewTitle)
+}
+
+func (suite *ConfigTestSuite) TestAuthStorageDefaultsFromFile() {
+	sut, err := config.NewFromFile("../../testdata/empty_testconfig.yaml")
+
+	assert.Nil(suite.T(), err)
+	assert.NotNil(suite.T(), sut)
+
+	assert.Equal(suite.T(), "yaml", sut.Auth.StorageMode)
+	assert.Equal(suite.T(), "./auth-storage.yaml", sut.Auth.Config["path"])
 }
