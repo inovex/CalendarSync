2e8c270bd30a0bbb4171144d0ae2a3e5c3a22fea
Alexander Huck <alexander.huck@inovex.de>
2024-04-21 14:52:54 +0200

    docs: add comment on what happens if the users enters invalid hours

--- internal/filter/timeFrame.go
+++ internal/filter/timeFrame.go
@@ -14,6 +14,7 @@ func (a TimeFrameEvents) Name() string {
 }
 
 func (a TimeFrameEvents) Filter(event models.Event) bool {
+	// If the user enters invalid hours, such as numbers higher than 24 or lower than 0, all events will be synchronized up to that point in time. For example, if the start hour is -1 and the end hour is 17, all events until 17 o'clock will be synchronized.
 	if event.StartTime.Hour() >= a.HourStart && event.StartTime.Hour() <= a.HourEnd {
 		return true
 	}
