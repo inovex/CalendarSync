b9c10ebb9c4eee00f7ec06c59e3c1f27642f0a09
Timon Engelke <timon.engelke@inovex.de>
2024-12-18 16:28:59 +0100

    logging: make default logging less verbose, add useful logs

--- cmd/calendarsync/main.go
+++ cmd/calendarsync/main.go
@@ -114,7 +114,7 @@ func Run(c *cli.Context) error {
 	if err != nil {
 		return err
 	}
-	log.Info("loaded config file", "path", cfg.Path)
+	log.Debug("loaded config file", "path", cfg.Path)
 
 	if len(c.String(flagStorageEncryptionKey)) > 0 {
 		log.Warn("Parsing the encryption key using the flag is deprecated. Please use the environment variable $CALENDARSYNC_ENCRYPTION_KEY instead.")
@@ -166,7 +166,7 @@ func Run(c *cli.Context) error {
 	if err != nil {
 		return err
 	}
-	log.Info("loaded source adapter", "adapter", cfg.Source.Adapter.Type, "calendar", cfg.Source.Adapter.Calendar)
+	log.Debug("loaded source adapter", "adapter", cfg.Source.Adapter.Type, "calendar", cfg.Source.Adapter.Calendar)
 
 	sinkLogger := log.With("adapter", cfg.Sink.Adapter.Type, "type", "sink")
 
@@ -181,7 +181,7 @@ func Run(c *cli.Context) error {
 	if err != nil {
 		return err
 	}
-	log.Info("loaded sink adapter", "adapter", cfg.Sink.Adapter.Type, "calendar", cfg.Sink.Adapter.Calendar)
+	log.Debug("loaded sink adapter", "adapter", cfg.Sink.Adapter.Type, "calendar", cfg.Sink.Adapter.Calendar)
 
 	// By default go runs a garbage collection once the memory usage doubles compared to the last GC run.
 	// Decrypting the storage in NewSourceAdapterFromConfig/NewSinkAdapterFromConfig requires a lot of memory,
